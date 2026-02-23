96e847f6bed85d7eb453f429a8013be6066f4166
desart <domdeorboc@gmail.com>
2026-02-23 22:56:02 +0800

    added file based compression in the options

--- main.c
+++ main.c
@@ -8,7 +8,8 @@ void menu_options(){
     printf("\n ***OPTIONS*** \n");
     printf("1. Compress\n");
     printf("2. Decompress\n");
-    printf("3. Exit\n");
+    printf("3. Compress (file-based)\n");
+    printf("5. Exit\n");
     printf("Enter your choice: ");
 
 }
@@ -27,15 +28,14 @@ int main(){
 
         switch (choice) {
             case 1:
+            char compressedText[256];
                 printf("Enter text to Compress: ");
                 fgets(input, sizeof(input), stdin);
                 input[strcspn(input, "\n")] = 0;
 
-                for (int i = 0; input[i]; i++) {
-                    input[i] = toupper(input[i]);
-                }
-
-                rleCompressor(input);
+                rleCompressor(input, compressedText);
+                
+                printf("Compressed Text: %s\n", compressedText);
                 break;
 
             case 2:
@@ -49,8 +49,61 @@ int main(){
 
                 rleDecompressor(input);
                 break;
-
             case 3:
+                char filename[256]; // input file name
+                char buffer[256];   // buffer for file content
+                char compressed[256];   // compressed result
+                char outputFilename[20]; // output file name
+
+                printf("\nEnter input file name: ");
+                scanf("%255s", filename);
+
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
+
+                FILE *file = fopen(filename, "r");
+                if(file == NULL){
+                    perror("File error");
+                    break;
+                }
+
+            
+                int ch = '\0';
+                int index = 0;
+                   
+                while((ch = fgetc(file)) != EOF && index < 256){
+                    buffer[index] = ch;
+                    index++;
+                }
+                   buffer[index] = '\0';
+                
+                fclose(file);
+
+                rleCompressor(buffer, compressed);
+                
+
+                printf("%s\n", compressed);
+
+                printf("\nEnter output file name: ");
+                scanf("%255s", outputFilename);  
+
+                while ((c = getchar()) != '\n' && c != EOF);
+
+                FILE *file2 = fopen(outputFilename, "w");
+                 if(file2 == NULL){
+                    perror("File error");
+                    break;
+                }
+
+                fprintf(file2, "%s", compressed);
+
+                fclose(file2);
+
+                printf("\nResult succesfully written to %s.\n", outputFilename);
+
+                break;
+
+            case 5:
                 printf("Exiting the program. Goodbye!\n");
                 return 0;
 
